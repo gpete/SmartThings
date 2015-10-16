4962a846af2874f8f4c527d674a8b2cee417ebd2
Greg Peterson <greg.peterson@aggiemail.usu.edu>
2015-10-16 09:12:02 -0600

    Fixed code storage within device. Fixed tile display text.

--- devicetypes/gpete/433-outlet.src/433-outlet.groovy
+++ devicetypes/gpete/433-outlet.src/433-outlet.groovy
@@ -7,15 +7,12 @@
 metadata {
 	definition (name: "433 Outlet", namespace: "gpete", author: "Greg Peterson") {
 		capability "Switch"
-        
-        attribute "onCode", "string"
-        attribute "offCode", "string"
 	}
 
     tiles {
         standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-            state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-            state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+            state "off", label: "Off", action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+            state "on", label: "On", action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
         }
         main "switch"
         details(["switch","on","off"])
@@ -23,8 +20,8 @@ metadata {
 }
 
 def init(Map attributes) {
-	sendEvent(name: "onCode", value: attributes.code)
-    sendEvent(name: "offCode", value: attributes.code2)
+	state.onCode = attributes.code
+    state.offCode = attributes.code2
 }
 
 def rename(newName) {
@@ -38,8 +35,8 @@ def parse(String description) {
 
 def on() {
     log.debug "Executing on()"
-    if (device.onCode) {
-        def result = parent.sendRFCommand(device.onCode)
+    if (state.onCode) {
+        def result = parent.sendRFCommand(state.onCode)
         log.debug "Result: $result"
         sendEvent(name: "switch", value: "on")
     }
@@ -47,8 +44,8 @@ def on() {
 
 def off() {
     log.debug "Executing off()"
-    if (device.offCode) {
-        def result = parent.sendRFCommand(device.offCode)
+    if (state.offCode) {
+        def result = parent.sendRFCommand(state.offCode)
         log.debug "Result: $result"
         sendEvent(name: "switch", value: "off")
     }
